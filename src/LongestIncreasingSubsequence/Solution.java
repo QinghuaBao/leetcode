cc42b415ac05e5b0f9cd683666f1359df4c08e04
foam <1060762481@qq.com>
2018-03-27 15:37:43 +0800

    300. Longest Increasing Subsequence

---
+++ src/LongestIncreasingSubsequence/Solution.java
@@ -0,0 +1,31 @@
+package LongestIncreasingSubsequence;
+
+/**
+ * Created by bqh on 2018/3/26.
+ * <p>
+ * E-mail:M201672845@hust.edu.cn
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {1,3,6,7,9,4,10,5,6};
+        System.out.println(lengthOfLIS(nums));
+    }
+
+    public static int lengthOfLIS(int[] nums) {
+        if (nums.length == 0){
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
+    }
+}
